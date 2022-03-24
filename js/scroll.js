9e8fcc4dfdb460bf984dcd2e87c9420c4fb79a98
AvetisHarutyunyan <avetis.harutyunyan.98@gmail.com>
2022-03-24 19:54:29 +0400

    added going to up button

--- js/scroll.js
+++ js/scroll.js
@@ -1,4 +1,7 @@
 window.addEventListener("scroll", () => {
     const topNav = document.querySelector(".header__section");
     topNav.classList.toggle("active-navbar", scrollY > 100);
+
+    const toUpBtn = document.querySelector(".toUpBtn");
+    toUpBtn.classList.toggle("show-toUpBtn", scrollY > 0);
 });
