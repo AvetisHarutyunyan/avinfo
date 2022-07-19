7a37334aac7ee754b79884f9c59c5d6f9c01749f
Avetis Harutyunyan <avetis.harutyunyan.98@gmail.com>
2022-07-19 14:51:00 +0400

    update AvInfo

--- js/scroll.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("scroll", () => {
-    const topNav = document.querySelector(".header__section");
-    topNav.classList.toggle("active-navbar", scrollY > 100);
-
-    const toUpBtn = document.querySelector(".toUpBtn");
-    toUpBtn.classList.toggle("show-toUpBtn", scrollY > 800);
-});
