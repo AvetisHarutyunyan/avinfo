238e9ee488f2a08336cc3cd64b4d09282322675c
Avetis Harutyunyan <avetis.harutyunyan.98@gmail.com>
2023-04-22 14:41:28 +0400

    added player

--- src/js/app.js
+++ src/js/app.js
@@ -45,3 +45,22 @@ new Swiper(".project__items", {
         }
     }
 });
+
+function playStopBtn() {
+    const playStopToggle = document.querySelector(".play_and_stop__btn");
+    const playStopText = document.querySelector(".play_and_stop__text");
+    const music = document.querySelector(".music_mp3");
+
+    playStopToggle.addEventListener("click", () => {
+        if (!playStopToggle.classList.contains("songStop")) {
+            playStopToggle.classList.add("songStop");
+            playStopText.textContent = "Play";
+            music.pause();
+        } else {
+            playStopToggle.classList.remove("songStop");
+            playStopText.textContent = "Stop";
+            music.play();
+        }
+    });
+}
+playStopBtn();
